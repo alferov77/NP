e81c053275d8fd08ff2d3c58e67e6d22c42fe5a8
Andrej Alferov <alferov77@gmail.com>
2024-05-11 13:15:57 +0200

    Домашнее задание модуля D11

--- NewsPortal/manage.py
+++ NewsPortal/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.base import BaseCommand
 
 def main():
     """Run administrative tasks."""
@@ -17,6 +17,34 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+class Command(BaseCommand):
+    help = 'Удаление публикаций'
+
+    def add_arguments(self, parser):
+        parser.add_argument('category', type=str)
+        parser.add_argument(
+            '--yes',
+            action='store_true',
+            help='Подтвердить удаление без запроса подтверждения'
+        )
+
+    def handle(self, *args, **options):
+        # Импортируем модели только когда это необходимо, внутри метода
+        from news.models import Post, Category
+
+        if not options['yes']:
+            answer = input(f'Вы правда хотите удалить все статьи в категории {options["category"]}? yes/no: ')
+            if answer != 'yes':
+                self.stdout.write(self.style.ERROR('Отменено'))
+                return
+
+        try:
+            category = Category.objects.get(name=options['category'])
+            Post.objects.filter(category=category).delete()
+            self.stdout.write(self.style.SUCCESS(
+                f'Успешно удалены все новости из категории {category.name}'))
+        except Category.DoesNotExist:
+            self.stdout.write(self.style.ERROR(f'Категория "{options["category"]}" не существует'))
 
 if __name__ == '__main__':
     main()
--- NewsPortal/news/admin.py
+++ NewsPortal/news/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment
 
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Post)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+# Inline класс для PostCategory, чтобы управлять категориями из админки постов
+class PostCategoryInline(admin.TabularInline):
+    model = PostCategory
+    extra = 1  # Количество пустых форм для добавления новых категорий
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'creation_date', 'post_type')
+    list_filter = ('post_type', 'creation_date')
+    search_fields = ('title', 'text')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'text', 'author', 'post_type')
+        }),
+    )
+    inlines = [PostCategoryInline]  # Добавление inline для категорий
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'creation_date', 'text')
+    list_filter = ('creation_date', 'user')
+    search_fields = ('text',)
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
